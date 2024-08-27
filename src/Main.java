e18e3eaad0b96e74e7f829c4c8526b1aa98e54a0
Fabricio Valadares <fabricio.valadares@gmail.com>
2024-08-27 15:05:50 -0300

    First commit

---
+++ src/Main.java
@@ -0,0 +1,16 @@
+
+
+public class Main {
+    public static void main(String[] args) {
+        UniBHList<Integer> myList = new UniBHList<>();
+
+        for (int i = 0; i < 10; i++) {
+            myList.insertAtBeginning(i + 1);
+            System.out.println(myList);
+        }
+
+        System.out.println(myList);
+
+        System.out.println();
+    }
+}
---
+++ src/UniBHList.java
@@ -0,0 +1,41 @@
+public class UniBHList<T> {
+    // Hold the reference to the first node of this List.
+    private Node<T> firstNode;
+    private int totalElements;
+
+    public void insertAtBeginning(T value) {
+        Node<T> newNode = new Node<T>(value);
+        newNode.setNext(firstNode);
+        firstNode = newNode;
+        totalElements++;
+    }
+
+    public Node<T> removeAtBeginning() {
+        Node<T> aux = firstNode;
+        firstNode = firstNode.getNext();
+        totalElements--;
+        return aux;
+    }
+
+    @Override
+    public String toString() {
+        if(this.totalElements == 0) {
+            return "[ ]";
+        }
+
+        Node<T> currentNode = firstNode;
+        StringBuilder builder = new StringBuilder("[");
+
+        for (int i = 0; i < totalElements; i++) {
+            builder.append(currentNode.getValue());
+            builder.append(", ");
+
+            currentNode = currentNode.getNext();
+
+        }
+
+        builder.append("]");
+
+        return  builder.toString();
+    }
+}
